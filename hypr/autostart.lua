4f9e110f152b24b6e204f3ebf9528bc03e5e14aa
lev2010ermakov-bit <lev2010ermakov@gmail.com>
2026-09-10 16:56:21 +0500

    small additionals

--- hypr/autostart.lua
+++ hypr/autostart.lua
@@ -1,7 +1,7 @@
 -- AUTOSTART
 hl.on("hyprland.start", function ()
   hl.exec_cmd('matugen color hex "#ffffff" --import-json "~/.config/matugen/themes/mountain-sunset.json"')
-  hl.exec_cmd('mpvpaper -o "loop panscan=1" ALL "~/.local/share/wallpapers/autumn-mountain-sunset.mp4" &>/dev/null &') 
+  hl.exec_cmd('mpvpaper -o "loop panscan=1 hwdec=auto-safe vo=gpu" ALL "~/.local/share/wallpapers/autumn-mountain-sunset.mp4" &>/dev/null &') 
   hl.exec_cmd("waybar &>/dev/null &")
   hl.exec_cmd("mako")
 end)
